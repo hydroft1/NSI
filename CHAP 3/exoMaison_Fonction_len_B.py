30b7415c18b60cbdc2780d1d79ca9893e4caf175
hydroft1 <113243914+hydroft1@users.noreply.github.com>
2022-11-20 10:41:00 +0100

    Add files via upload

---
+++ CHAP 3/exoMaison_Fonction_len_B.py
@@ -0,0 +1,16 @@
+﻿# écrivez la fonction taille() qui sera égale à la fonction len()
+
+def taille(mot_loc):
+    total = 0
+    for element in mot_loc:
+        total = total + 1
+    return total
+    
+    
+    
+    
+mot_loc="toto"
+cpt=taille(mot_loc)
+print("Le nombre de caractères de la chaine mot est :",cpt)
+
+
