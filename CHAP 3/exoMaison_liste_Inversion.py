0e20e8449f7608cf1013f14544310d1701d6b704
hydroft1 <113243914+hydroft1@users.noreply.github.com>
2022-12-04 11:07:08 +0100

    Add files via upload

---
+++ CHAP 3/exoMaison_liste_Inversion.py
@@ -0,0 +1,21 @@
+﻿# Ecrivez un programme qui inverse les éléments d'une liste L1 dans une nouvelle liste L2
+# vous n'utiliserez pas la méthode reverse().
+# Exemple :
+# Entrée : L1 = [5,6,7,8,9]
+# Sortie : L2 = [9,8,7,6,5]
+""""
+L1=[5,6,7,8,9]
+L2=[]
+for i in range(len(L1)):
+    L2.append(L1[-i-1])
+print(L2)
+"""
+# Ecrivez un programme qui inverse les éléments d'une liste L (sans utiliser une nouvelle liste).
+# vous n'utiliserez pas la méthode reverse().
+# Exemple :
+# Entrée : L = [5,6,7,8,9]
+# Sortie : L = [9,8,7,6,5]
+"""
+L=[5,6,7,8,9]
+print(list(reversed(L)))
+"""
