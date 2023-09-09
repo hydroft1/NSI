a16b27b23e2e073b7653d1bd47479df5c56998d5
hydroft1 <marquetalexandre@proton.me>
2023-09-09 12:58:59 +0200

    ajout de la docstring avec des test

--- Terminal/est_permut.py
+++ Terminal/est_permut.py
@@ -1,4 +1,25 @@
 def est_permut(tab):
+    """
+    Vérifie si une liste donnée contient une permutation des entiers de 1 à n.
+
+    Args:
+        tab (list): Une liste d'entiers.
+
+    Returns:
+        bool: True si la liste est une permutation, False sinon.
+
+    Examples:
+        >>> est_permut([1, 2, 3, 4])
+        True
+        >>> est_permut([4, 3, 2, 1])
+        True
+        >>> est_permut([1, 2, 2, 4])
+        False
+        >>> est_permut([1, 2, 3, 5])
+        False
+        >>> est_permut([])
+        False
+    """
     n = len(tab)
     appartenance = [False] * n
     
@@ -10,6 +31,3 @@ def est_permut(tab):
         indice += 1
     
     return indice == n
-
-
-
