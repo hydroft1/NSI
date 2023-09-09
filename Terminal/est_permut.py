eb3059e24155fe2090f98a713d2635a8394cc84d
hydroft1 <marquetalexandre@proton.me>
2023-09-09 16:02:37 +0200

    Ajustement

--- Terminal/est_permut.py
+++ Terminal/est_permut.py
@@ -2,23 +2,22 @@ def est_permut(tab):
     """
     Vérifie si une liste donnée contient une permutation des entiers de 1 à n.
 
-    Args:
+    Entrée:
         tab (list): Une liste d'entiers.
 
-    Returns:
+    Sortie:
         bool: True si la liste est une permutation, False sinon.
 
-    Examples:
-        >>> est_permut([1, 2, 3, 4])
-        True
-        >>> est_permut([4, 3, 2, 1])
-        True
-        >>> est_permut([1, 2, 2, 4])
-        False
-        >>> est_permut([1, 2, 3, 5])
-        False
-        >>> est_permut([])
-        False
+    >>> est_permut([1, 2, 3, 4])
+    True
+    >>> est_permut([4, 3, 2, 1])
+    True
+    >>> est_permut([1, 2, 2, 4])
+    False
+    >>> est_permut([1, 2, 3, 5])
+    False
+    >>> est_permut([])
+    False
     """
     n = len(tab)
     appartenance = [False] * n
