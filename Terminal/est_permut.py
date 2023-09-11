e148c2d273be75d50ed36bd920aa88c75b6e28d8
hydroft1 <marquetalexandre@proton.me>
2023-09-11 20:01:32 +0200

    Tags

--- Terminal/est_permut.py
+++ Terminal/est_permut.py
@@ -1,3 +1,5 @@
+# hydroft1
+
 def est_permut(tab):
     """
     Vérifie si une liste donnée contient une permutation des entiers de 1 à n.
@@ -7,6 +9,17 @@ def est_permut(tab):
 
     Sortie:
         bool: True si la liste est une permutation, False sinon.
+        
+    >>> est_permut([1, 2, 3, 4])
+    True
+    >>> est_permut([4, 3, 2, 1])
+    True
+    >>> est_permut([1, 2, 2, 4])
+    False
+    >>> est_permut([1, 2, 3, 5])
+    False
+    >>> est_permut([])
+    False
     """
     n = len(tab)
     appartenance = [False] * n
