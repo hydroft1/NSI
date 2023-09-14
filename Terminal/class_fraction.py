8e46538ad4e3565ebcbc09191adf90d9dfc4809b
hydroft1 <marquetalexandre@proton.me>
2023-09-14 09:09:26 +0200

    update

--- Terminal/class_fraction.py
+++ Terminal/class_fraction.py
@@ -24,6 +24,14 @@ class Fraction:
         d = frc.denominateur * self.denominateur
         return Fraction(n, d)
         
+    def mystere(self, a, b):
+        if a < b:
+            a, b = b, a
+        
+        while b > 0:
+            a, b = b, a % b
+        
+        return a
     
     def __str__(self):
         return str(self.numerateur) + '/' + str(self.denominateur)
@@ -33,3 +41,32 @@ g = Fraction(-5, 6)
 h = Fraction(8, 7)
 
 print(f + g + h)
+
+
+"""
+helpers.py -> module de méthode pratique
+PG CD -> Plus grand Commun Diviseur ( de deux nombres)
+
+Méthode 1 : sur a = 18 et b = 45
+18 = 1x18   45 = 1x45
+18 = 2x9    45 = 3x15
+18 = 3x6    45 = 5x9
+('1', '3', 6, `'9'`, 18)    ('1','3',5,`'9'`,15,45)
+' diviseur commun
+` PGCD
+18/45 = 9x2 / 9x5 = 2/5 
+
+Méthode 2 : si d divise a )
+            et d divise b ) -> alors d divise ( a + b et
+                                              ( a - b
+--------------------------------------------------                                             
+| 45 - 18 = 27                                   | 
+| d divise 45, (18 et 27) -> deux plus petits    |
+|                                                | Méthode 3 : 45 - 2 x 18 = 9 -> reste / division euclidéenne 
+| 27 - 18 = 9                                    |
+| d divise 45, 27, (18 et 9) -> deux plus petits |
+--------------------------------------------------
+18 - 9 = 9
+d divise 45, 27, 18 et (9) => `plus petits trouvé` 
+                                            
+"""
