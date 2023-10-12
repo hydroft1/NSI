e737c5ee2fdf4b9ecc711c502c3a5338fefbe074
hydroft1 <marquetalexandre@proton.me>
2023-10-12 08:57:30 +0200

    exo 3 et 2

--- Terminal/exo_recursivite/exercice2.py
+++ Terminal/exo_recursivite/exercice2.py
@@ -20,6 +20,25 @@ def inversion(tab, inverse):
     return inversion(tab[1:], [tab[0]] + inverse)
     
 
+def inversion_v2(tab, i=0):
+    """ inversion en place du tableau
+    
+    >>> T = [0, 1, 2, 3, 4]
+    >>> inversion_v2(T)
+    >>> T
+    [4, 3, 2, 1]
+    >>> T = [0, 1, 2, 3]
+    >>> inversion_v2(T)
+    >>> T
+    [3, 2, 1, 0]
+    
+    """
+    n = len(tab)
+    if i == n // 2:
+        ...
+    tab[i], tab[n - 1 - i] = tab[n - 1 - i], tab[i]
+    inversion_v2(tab, i + 1)
+       
 
 if __name__ == '__main__':
     import doctest
--- Terminal/exo_recursivite/exercice3.py
+++ Terminal/exo_recursivite/exercice3.py
@@ -1,19 +1,18 @@
-def mediane(tableau):
-    n = len(tableau)
+def mediane(tab, i=0):
+    """
+    >>> T = [0, 1, 2, 3, 4]
+    >>> mediane(T)
+    2.0
+    >>> T = [0, 1, 2, 3]
+    >>> mediane(T)
+    1.5
     
-    if n % 2 == 1:
-        # Si le nombre d'éléments est impair, la médiane est au milieu.
-        mediane = tableau[n // 2]
-    else:
-        # Si le nombre d'éléments est pair, la médiane est la moyenne des deux éléments au milieu.
-        milieu_gauche = tableau[n // 2 - 1]
-        milieu_droite = tableau[n // 2]
-        mediane = (milieu_gauche + milieu_droite) / 2
-    
-    return mediane
+    """
+    n = len(tab)
+    if n <= 2:
+        return (tab[0] + tab[n -1]) / 2
+    return mediane(tab[1:n - 1])
 
-
-T = [1, 2, 3, 4, 5]
-T.sort()  
-med = mediane(T)
-print("La médiane est :", med)
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
