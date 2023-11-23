2bbea6c44cd90935911679cae3c26ff45b96cc88
hydroft1 <marquetalexandre@proton.me>
2023-11-23 09:55:28 +0100

    commit

--- Terminal/tri_fusion.py
+++ Terminal/tri_fusion.py
@@ -1,3 +1,10 @@
 def fusion(tab1, tab2):
-    i1 = i2 = 0
+    resultat = []
+    i1, i2 = 0, 0
+    while i1 < len(tab1) and i2 < len(tab2):
+        pass
     
+    if i1 == len(tab1):
+        pass
+    else:
+        pass
