7ce1b9b5edc7657b1933b3323d9b8222dd8b71b2
hydroft1 <marquetalexandre@proton.me>
2023-12-14 09:26:21 +0100

    commit

--- Terminal/décorateurs.py
+++ Terminal/décorateurs.py
@@ -1,11 +1,25 @@
 import time
 
-
+def temps(affichage):    
+    def _temps(f):
+        def g(*args):
+            t0 = time.time_ns()
+            sortie = f(*args)
+            t1 = time.time_ns()
+            if affichage:
+                print((t1 - t0) * 1e-9 )
+            return sortie
+        
+        return g
+    if affichage:
+        return _temps
+    else:
+        return lambda f: f
+    
+    
+@temps(False)
 def mafonction(x):
     return x ** 2
 
-t0 = time.time_ns()
 sortie = mafonction(2)
-t1 = time.time_ns()
-print((t1 - t0) * 1e-9 )
 print(sortie)
