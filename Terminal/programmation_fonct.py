ce4d9d56c1bfa664545d458783413e60fb3a4881
hydroft1 <marquetalexandre@proton.me>
2024-01-11 09:44:28 +0100

    create programmation fonctionnelle

---
+++ Terminal/programmation_fonct.py
@@ -0,0 +1,19 @@
+def car(tab):
+    """ Renvoie le premier élément d'un tableau non vide
+    """
+    return tab[0]
+    
+def cdr(tab):
+    """ Renvoie un tableau des éléments d'un tableau non vide sauf le premier
+    """
+    # version sans slice
+    
+    
+    # version avec slice
+    #return tab[1:]
+    
+def cons(elt, tab):
+    ...
+
+
+
