fa36a00d23fa117c7a6620639709ffad26a6dbd8
hydroft1 <marquetalexandre@proton.me>
2024-03-14 09:15:41 +0100

    creation abr pédigrée

---
+++ Terminal/arbre_pedigrée.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+
+class File:
+    def __init__(self):
+        self.mafile = deque()
+        
+    
+class Noeud:
+    ...
+    
+def nombre_chiens(arb, n):
+    ...
+    
+nombre_chiens = Noeud()
